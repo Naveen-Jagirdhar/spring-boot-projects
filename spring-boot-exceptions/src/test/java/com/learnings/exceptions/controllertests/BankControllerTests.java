2c5879262a071099cfbb98e7c9e132d43f8b4e26
Naveen Jagirdhar <jagirdharnaveen69@gmail.com>
2020-06-06 16:36:46 +0530

    Update BankControllerTests.java

--- spring-boot-exceptions/src/test/java/com/learnings/exceptions/controllertests/BankControllerTests.java
+++ spring-boot-exceptions/src/test/java/com/learnings/exceptions/controllertests/BankControllerTests.java
@@ -18,6 +18,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.Arrays;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -62,15 +63,32 @@ public class BankControllerTests {
 
     @Test
     public void getBankDetailsTest() throws Exception {
-        when(services.fetchCompleteDetails()).thenReturn(Arrays.asList(
-                new Banks("21212063-e6ea-47da-a4f2-f9d9bf53e346","Soft Bank","1"),
-                new Banks("f0c08a07-54a9-4955-803c-3a6860eed15d","SBI Bank","2")
-        ));
+        when(
+                services.fetchCompleteDetails())
+                .thenReturn(Arrays.asList(
+                        new Banks("21212063-e6ea-47da-a4f2-f9d9bf53e346", "Soft Bank", "1"),
+                        new Banks("f0c08a07-54a9-4955-803c-3a6860eed15d", "SBI Bank", "2")))
+                .thenReturn(Arrays.asList());
 
-        RequestBuilder requestBuilder  = MockMvcRequestBuilders.get("/banks")
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/banks")
                 .accept(MediaType.APPLICATION_JSON_VALUE);
 
         mockMvc.perform(requestBuilder)
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getBanksTest() throws Exception {
+        when(services.fetchBankDetails("21212063-e6ea-47da-a4f2-f9d9bf53e346"))
+                .thenReturn(new Banks("21212063-e6ea-47da-a4f2-f9d9bf53e346", "Soft Bank", "1"));
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/banks/21212063-e6ea-47da-a4f2-f9d9bf53e346")
+                .accept(MediaType.APPLICATION_JSON_VALUE);
+
+        mockMvc.perform(requestBuilder).andExpect(status().isOk())
+                .andExpect(jsonPath("$.bankNo").value("21212063-e6ea-47da-a4f2-f9d9bf53e346"))
+                .andExpect(jsonPath("$.bankName").value("Soft Bank"))
+                .andExpect(jsonPath("$.bankId").value("1"));
+        ;
     }
 }
