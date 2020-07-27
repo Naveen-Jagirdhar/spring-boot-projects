7cd024e4420069c436ec8bfb4a8edb381be8011c
Naveen <jagirdharnaveen69@gmail.com>
2020-07-27 18:05:11 +0530

    profiles

---
+++ sb-profiles-pom/src/main/java/com/qworks/sbprofilessample/SbProfilesSampleApplication.java
@@ -0,0 +1,13 @@
+package com.qworks.sbprofilessample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbProfilesSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbProfilesSampleApplication.class, args);
+	}
+
+}
---
+++ sb-profiles-pom/src/test/java/com/qworks/sbprofilessample/SbProfilesSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qworks.sbprofilessample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbProfilesSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
